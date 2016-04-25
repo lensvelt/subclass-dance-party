979ee0bb7b51a18554e24dca7611c4c2eefe132f
lensvelt <lensvelt.brett@gmail.com>
2016-04-25 16:15:39 -0700

    Updated to psuedoclassical style with other various style changes

--- src/blinkyDancer.js
+++ src/blinkyDancer.js
@@ -1,19 +1,28 @@
-var makeBlinkyDancer = function(top, left, timeBetweenSteps) {
-  var blinkyDancer = makeDancer(top, left, timeBetweenSteps);
-
+var BlinkyDancer = function(top, left, timeBetweenSteps) {
   // we plan to overwrite the step function below, but we still want the superclass step behavior to work,
   // so we must keep a copy of the old version of this function
+  this.$node = $('<span class="blinky-dancer"></span>');
+  this.step(timeBetweenSteps);
+  this.setPosition(top, left);
+};
 
-  var oldStep = blinkyDancer.step;
+BlinkyDancer.prototype = Object.create(Dancer.prototype);
+BlinkyDancer.prototype.constructor = BlinkyDancer;
 
-  blinkyDancer.step = function() {
-    // call the old version of step at the beginning of any call to this new version of step
-    oldStep();
-    // toggle() is a jQuery method to show/hide the <span> tag.
-    // See http://api.jquery.com/category/effects/ for this and
-    // other effects you can use on a jQuery-wrapped html tag.
-    blinkyDancer.$node.toggle();
+BlinkyDancer.prototype.step = function(timeBetweenSteps) {
+  // call the old version of step at the beginning of any call to this new version of step
+
+  Dancer.prototype.step.call(this, timeBetweenSteps);
+  // toggle() is a jQuery method to show/hide the <span> tag.
+  // See http://api.jquery.com/category/effects/ for this and
+  // other effects you can use on a jQuery-wrapped html tag.
+  // this.$node.toggle();
+  var opactiyMeasure = Math.random();
+  var blinkyStyleSettings = {
+    opacity: opactiyMeasure
   };
 
-  return blinkyDancer;
+  this.$node.toggle();
+  this.$node.css(blinkyStyleSettings);
 };
+
